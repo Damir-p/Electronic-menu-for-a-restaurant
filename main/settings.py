1ba73b2b9476d085e33a30488941d6b44c7cbe95
Damir-p <damir.ptn@gmail.com>
2023-05-25 19:18:57 +0600

    create order.models

--- main/settings.py
+++ main/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'accounts',
     'menu',
     'cart',
+    'orders',
 ]
 
 MIDDLEWARE = [
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from orders.models import Order
 
-# Create your views here.
+class OrderView(View):
+
+    def get(self , request ):
+        customer = request.session.get('customer')
+        orders = Order.get_orders_by_customer(customer)
+        print(orders)
+        return render(request , 'orders.html'  , {'orders' : orders})
