d5d0290329b5bdb22426506d9b48a41f7c2ef7ce
Damir-p <damir.ptn@gmail.com>
2023-06-04 20:40:49 +0600

    config

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
-# from apps.orders.views import AddToCartView, CartView, CheckoutView
+from apps.orders.views import AddToCartView, CartView, CheckoutView
 
-
-# urlpatterns = [
-#     path('add-to-cart/', AddToCartView.as_view(), name='add_to_cart'),
-#     path('', CartView.as_view(), name='cart'),
-#     path('checkout/', CheckoutView.as_view(), name='checkout'),
-# ]
 
 urlpatterns = [
-    path('accounts/login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('add-to-cart/', AddToCartView.as_view(), name='add_to_cart'),
+    path('', CartView.as_view(), name='cart'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
 ]
+
